5bac54c478473171d01d9d8de4c528bbcbce85bc
Dev Singh <dsingh14@illinois.edu>
2024-11-12 23:46:18 -0600

    cache role response on client

--- src/routes/protected.ts
+++ src/routes/protected.ts
@@ -1,6 +1,12 @@
 import { FastifyPluginAsync } from "fastify";
+import fastifyCaching from "@fastify/caching";
 
 const protectedRoute: FastifyPluginAsync = async (fastify, _options) => {
+  fastify.register(fastifyCaching, {
+    privacy: fastifyCaching.privacy.PRIVATE,
+    serverExpiresIn: 0,
+    expiresIn: 60 * 15,
+  });
   fastify.get("/", async (request, reply) => {
     const roles = await fastify.authorize(request, reply, []);
     reply.send({ username: request.username, roles: Array.from(roles) });
